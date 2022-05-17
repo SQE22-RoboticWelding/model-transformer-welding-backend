a4e373def9528db646aa370979b16914b0dd3ce6
János Brodbeck <janos.brodbeck@posteo.de>
2022-05-17 23:15:17 +0200

    make settings configurable by environment variables
    required to access the database inside docker containers and from localhost

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,13 +1,13 @@
-from typing import Any, Optional
+import os
 from pydantic import AnyHttpUrl, BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SERVER_NAME: str = "sqe_backend"
-    SERVER_HOST: AnyHttpUrl = "http://localhost"
-    PROJECT_NAME: str = "SQE-Welding-Project"
+    SERVER_NAME: str = os.getenv("SERVER_NAME", "sqe_backend")
+    SERVER_HOST: AnyHttpUrl = os.getenv("SERVER_HOST", "http://localhost")
+    PROJECT_NAME: str = os.getenv("PROJECT_NAME", "SQE-Welding-Project")
     # ToDo: store and access secrets properly
-    DATABASE_URL_ASYNC: str = "postgresql+asyncpg://postgres:awesomepw@localhost:5432"
-    DATABASE_URL_SYNC: str = "postgresql://postgres:awesomepw@localhost:5432"
+    DATABASE_URL_ASYNC: str = os.getenv("DATABASE_URL_ASYNC", f"postgresql+asyncpg://postgres:awesomepw@localhost:5432")
+    DATABASE_URL_SYNC: str = os.getenv("DATABASE_URL_SYNC", "postgresql://postgres:awesomepw@localhost:5432")
 
 settings = Settings()
