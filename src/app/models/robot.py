febc654a691ed936c8d27b8a9960fded11d5a17a
János Brodbeck <janos.brodbeck@posteo.de>
2022-05-30 01:39:05 +0200

    model: implement robot, robot_type, welding_configuration, welding_point

---
+++ src/app/models/robot.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
+
+
+class Robot(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    robot_type_id = Column(Integer, ForeignKey("robottype.id"))
+    description = Column(String)
+    robot_type = relationship("RobotType", lazy='joined')
