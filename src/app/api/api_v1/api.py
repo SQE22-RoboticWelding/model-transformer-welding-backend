ec6ac13e4f88f2cde9e78d5351d560c45c7c9352
János Brodbeck <janos.brodbeck@posteo.de>
2022-05-30 02:55:26 +0200

    endpoints: add robot

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import test, robot_type
+from app.api.api_v1.endpoints import test, robot, robot_type
 
 api_router = APIRouter()
 api_router.include_router(test.router, prefix="/test", tags=["test"])
 api_router.include_router(robot_type.router, prefix="/robottype", tags=["robotType"])
+api_router.include_router(robot.router, prefix="/robot", tags=["robot"])
---
+++ src/app/api/api_v1/endpoints/robot.py
@@ -0,0 +1,77 @@
+from fastapi import APIRouter, Depends, HTTPException
+from app.api import deps
+from app.crud.crud_robot import *
+from app.schemas.robot import *
+
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[Robot])
+async def read_robots(
+        db: AsyncSession = Depends(deps.get_async_db),
+        skip: int = 0,
+        limit: int = 100,
+) -> Any:
+    """
+    Retrieve multiple robot types
+    """
+    result = await robot.get_multi(db=db, skip=skip, limit=limit)
+    return result
+
+
+@router.get("/{id}", response_model=Robot)
+async def read_robot(
+        *,
+        db: AsyncSession = Depends(deps.get_async_db),
+        _id: int
+) -> Any:
+    """
+    Retrieve robot type by ID
+    """
+    result = await robot.get_by_id(db=db, id=_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Robot not found")
+    print(result.robot_type.name)
+    return result
+
+
+@router.post("/", response_description="Add new robot type", response_model=Robot)
+async def create_robot(
+        *,
+        db: AsyncSession = Depends(deps.get_async_db),
+        robot_in: RobotCreate):
+    """
+    Create new robot type
+    """
+    result = await robot.create(db=db, obj_in=robot_in)
+    return result
+
+
+@router.put("/{id}", response_model=Robot)
+async def update_robot(
+        *,
+        id: int,
+        db: AsyncSession = Depends(deps.get_async_db),
+        robot_in: RobotUpdate
+) -> Any:
+    """
+    Update a robot type
+    """
+    result = await robot.get_by_id(db=db, id=id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Robot not found")
+    return await robot.update(db=db, db_obj=result, obj_in=robot_in)
+
+
+@router.delete("/{id}", response_model=Robot)
+async def delete_robot(
+        *,
+        db: AsyncSession = Depends(deps.get_async_db),
+        _id: int
+) -> Any:
+    result = await robot.get_by_id(db=db, id=_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Robot not found")
+    result = await robot.remove(db=db, obj=result)
+    return result
