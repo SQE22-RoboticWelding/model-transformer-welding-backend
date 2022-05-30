4b39f734812884704384559c3c492508c037103d
János Brodbeck <janos.brodbeck@posteo.de>
2022-05-30 03:22:29 +0200

    endpoints: add welding_configuration, welding_point

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import test, robot, robot_type
+from app.api.api_v1.endpoints import test, robot, robot_type, welding_configuration, welding_point
 
 api_router = APIRouter()
+api_router.include_router(robot_type.router, prefix="/robottype", tags=["RobotType"])
+api_router.include_router(robot.router, prefix="/robot", tags=["Robot"])
+api_router.include_router(welding_configuration.router, prefix="/weldingconfiguration", tags=["WeldingConfiguration"])
+api_router.include_router(welding_point.router, prefix="/weldingpoint", tags=["WeldingPoint"])
 api_router.include_router(test.router, prefix="/test", tags=["test"])
-api_router.include_router(robot_type.router, prefix="/robottype", tags=["robotType"])
-api_router.include_router(robot.router, prefix="/robot", tags=["robot"])
---
+++ src/app/api/api_v1/endpoints/welding_point.py
@@ -0,0 +1,76 @@
+from fastapi import APIRouter, Depends, HTTPException
+from app.api import deps
+from app.crud.crud_welding_point import *
+from app.schemas.welding_point import *
+
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[WeldingPoint])
+async def read_welding_points(
+        db: AsyncSession = Depends(deps.get_async_db),
+        skip: int = 0,
+        limit: int = 100,
+) -> Any:
+    """
+    Retrieve multiple robot types
+    """
+    result = await welding_point.get_multi(db=db, skip=skip, limit=limit)
+    return result
+
+
+@router.get("/{id}", response_model=WeldingPoint)
+async def read_welding_point(
+        *,
+        db: AsyncSession = Depends(deps.get_async_db),
+        _id: int
+) -> Any:
+    """
+    Retrieve robot type by ID
+    """
+    result = await welding_point.get_by_id(db=db, id=_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Welding point not found")
+    return result
+
+
+@router.post("/", response_description="Add new robot type", response_model=WeldingPoint)
+async def create_welding_point(
+        *,
+        db: AsyncSession = Depends(deps.get_async_db),
+        welding_point_in: WeldingPointCreate):
+    """
+    Create new robot type
+    """
+    result = await welding_point.create(db=db, obj_in=welding_point_in)
+    return result
+
+
+@router.put("/{id}", response_model=WeldingPoint)
+async def update_welding_point(
+        *,
+        id: int,
+        db: AsyncSession = Depends(deps.get_async_db),
+        welding_point_in: WeldingPointUpdate
+) -> Any:
+    """
+    Update a robot type
+    """
+    result = await welding_point.get_by_id(db=db, id=id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Welding point not found")
+    return await welding_point.update(db=db, db_obj=result, obj_in=welding_point_in)
+
+
+@router.delete("/{id}", response_model=WeldingPoint)
+async def delete_welding_point(
+        *,
+        db: AsyncSession = Depends(deps.get_async_db),
+        _id: int
+) -> Any:
+    result = await welding_point.get_by_id(db=db, id=_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Welding point not found")
+    result = await welding_point.remove(db=db, obj=result)
+    return result
