907bc0075c0d3a24d2a6335d4dea7efe1412cde2
dolevi-tub <d.genz@campus.tu-berlin.de>
2022-06-09 14:32:56 +0200

    add tests for code_generation

--- src/app/api/api_v1/endpoints/code_generation.py
+++ src/app/api/api_v1/endpoints/code_generation.py
@@ -1,5 +1,10 @@
 from typing import Any
 from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api import deps
 from app.crud.crud_generation_template import generation_template
 from app.crud.crud_welding_configuration import welding_configuration
 from app.codegen.code_generator import CodeGenerator
@@ -8,17 +13,22 @@ from app.codegen.code_generator import CodeGenerator
 router = APIRouter()
 
 
-@router.post("/manual-welding-points", response_model=str, deprecated=True)
+class RequestBodyGenerate(BaseModel):
+    generation_template_id: int
+    welding_configuration_id: int
+
+
+@router.post("/generate", response_model=str, deprecated=True)
 async def generate(
         *,
-        generation_template_id: int,
-        welding_configuration_id: int
+        db: AsyncSession = Depends(deps.get_async_db),
+        body: RequestBodyGenerate
 ) -> Any:
     """
     Generate code by manually passing in welding points
     """
-    gen_template = await generation_template.get_by_id(generation_template_id)
-    welding_config = await welding_configuration.get_by_id(welding_configuration_id)
+    gen_template = await generation_template.get_by_id(db=db, id=body.generation_template_id)
+    welding_config = await welding_configuration.get_by_id(db=db, id=body.welding_configuration_id)
     if not gen_template:
         raise HTTPException(status_code=404, detail="Generation template not found")
     elif not welding_config:
