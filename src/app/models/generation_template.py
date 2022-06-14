e152e62b6d5dfa04bc60e398883bf1b8e67d6b24
dolevi-tub <d.genz@campus.tu-berlin.de>
2022-06-14 09:24:49 +0200

    implement review change requests

--- src/app/models/generation_template.py
+++ src/app/models/generation_template.py
@@ -7,6 +7,6 @@ class GenerationTemplate(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
+    content = Column(String)
     created_at = Column(DateTime, server_default=func.now())
     modified_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
-    content = Column(String)
