8f6ae00642651ac6c4a7c218d3e83d4a0ff7c277
János Brodbeck <janos.brodbeck@posteo.de>
2022-06-14 07:01:53 +0200

    model: enable cascading deletes for welding configuration

--- src/app/models/welding_configuration.py
+++ src/app/models/welding_configuration.py
@@ -10,4 +10,5 @@ class WeldingConfiguration(Base):
     description = Column(String)
     created_at = Column(DateTime, server_default=func.now())
     modified_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
-    welding_points = relationship("WeldingPoint", back_populates="welding_configuration", lazy='subquery')
+    welding_points = relationship("WeldingPoint", back_populates="welding_configuration", lazy='subquery',
+                                  cascade="all, delete")
