0d7676aa1502a9d4b697b1faf1ac27065d0bf6e1
János Brodbeck <janos.brodbeck@posteo.de>
2022-06-18 23:59:58 +0200

    tests: unittest for 'generate' of the CodeGenerator

---
+++ src/app/tests/codegen/test_codegen_class.py
@@ -0,0 +1,24 @@
+import pytest
+
+from app.codegen.code_generator import CodeGenerator
+from app.models.generation_template import GenerationTemplate
+from app.models.project import Project
+from app.models.welding_point import WeldingPoint
+from app.tests.utils.utils import get_example_template
+
+
+def test_code_generation():
+    project_obj = Project(id=1, name="My welding project")
+    welding_point_obj = WeldingPoint(project_id=1,
+                                     welding_order=0,
+                                     x=10,
+                                     y=5.5,
+                                     z=0.25,
+                                     roll=0.35,
+                                     pitch=3,
+                                     yaw=0)
+    project_obj.welding_points = [welding_point_obj]
+    generation_template_obj = GenerationTemplate(id=1, name="My Template", content=get_example_template())
+
+    generated = CodeGenerator.generate(template=generation_template_obj, project=project_obj)
+    assert generated == "10, 5.5, 0.25 / 0.35, 3, 0 / 0"
