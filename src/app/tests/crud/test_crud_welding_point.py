747931b857b1f9677a91bff140addd14b2a644ea
János Brodbeck <janos.brodbeck@posteo.de>
2022-06-19 14:50:03 +0200

    tests: implement missing crud delete + cascade delete test for welding point

--- src/app/tests/crud/test_crud_welding_point.py
+++ src/app/tests/crud/test_crud_welding_point.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.project import Project
 from app.models.welding_point import WeldingPoint
 from app.crud.crud_welding_point import welding_point
+from app.crud.crud_project import project
 from app.schemas.welding_point import WeldingPointUpdate
 from app.tests.utils.models import create_welding_point, create_project, get_welding_point_create
 
@@ -65,3 +66,25 @@ async def test_crud_welding_point_update(database: AsyncSession):
 
     await welding_point.update(db=database, db_obj=welding_point_obj, obj_in=WeldingPointUpdate(name="modified again"))
     assert welding_point_obj.name == "modified again"
+
+
+async def test_crud_welding_point_delete(database: AsyncSession):
+    project_obj = await create_project(db=database)
+    welding_point_obj = await create_welding_point(db=database, project_obj=project_obj)
+
+    result = await welding_point.remove(db=database, obj=welding_point_obj)
+    assert isinstance(result, WeldingPoint)
+
+    result = await welding_point.get(db=database, id=welding_point_obj.id)
+    assert result is None
+
+
+async def test_crud_welding_point_delete_cascade(database: AsyncSession):
+    project_obj = await create_project(db=database)
+    welding_point_obj = await create_welding_point(db=database, project_obj=project_obj)
+
+    result = await project.remove(db=database, obj=project_obj)
+    assert isinstance(result, Project)
+
+    result = await welding_point.get(db=database, id=welding_point_obj.id)
+    assert result is None
