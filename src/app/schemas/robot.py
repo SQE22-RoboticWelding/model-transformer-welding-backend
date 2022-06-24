811f723d8d8411527787671514cb398860001be6
János Brodbeck <janos.brodbeck@posteo.de>
2022-06-24 02:12:52 +0200

    schemas: add RobotWithType for returning robots with its type directly

--- src/app/schemas/robot.py
+++ src/app/schemas/robot.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel
 
+from app.schemas.robot_type import RobotTypeBase
+
 
 class RobotBase(BaseModel):
     id: int
@@ -23,6 +25,16 @@ class RobotCreate(RobotBase):
 class RobotUpdate(RobotBase):
     id: Optional[int]
     robot_type_id: Optional[int]
+
+
+class RobotWithType(RobotBase):
+    robot_type: RobotTypeBase
+
+    @staticmethod
+    def factory(robot: RobotBase, robot_type: RobotTypeBase):
+        dict_obj = robot.as_dict()
+        dict_obj["robot_type"] = robot_type.as_dict()
+        return RobotWithType(**dict_obj)
 
 
 # Properties shared by models stored in DB
