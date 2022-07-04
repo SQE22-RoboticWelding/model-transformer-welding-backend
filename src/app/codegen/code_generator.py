109743f76a0b26a82f74de4dd0f6e04f1385f53f
János Brodbeck <janos.brodbeck@posteo.de>
2022-07-04 03:35:16 +0200

    codegen: adjust generate to accept a list of welding points instead of project

--- src/app/codegen/code_generator.py
+++ src/app/codegen/code_generator.py
@@ -1,10 +1,14 @@
 """code_generator.py handles codegen of models."""
+from typing import List
+
 from jinja2 import Template
+
+from app.models.welding_point import WeldingPoint
 from app.schemas.generation_template import GenerationTemplate
 from app.models.project import Project
 
 
 class CodeGenerator:
     @staticmethod
-    def generate(template: GenerationTemplate, project: Project) -> str:
-        return Template(template.content).render({"welding_points": project.welding_points})
+    def generate(template: GenerationTemplate, welding_points: List[WeldingPoint]) -> str:
+        return Template(template.content).render({"welding_points": welding_points})
